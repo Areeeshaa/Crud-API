dfa5ec8d47f49a5d940c1a5dc09b4479c0d3322d
Areesha <f230735@cfd.nu.edu.pk>
2026-07-19 02:15:01 +0500

    stage: 1 root and health endpoints

--- A1server.js
+++ A1server.js
@@ -4,6 +4,13 @@ app.get('/',(req,res)=>{
     res.send("Hello world")
 
 })
+app.get('/tasks', (req,res)=>{
+    res.send({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] })
+
+})
+app.get('/health', (req,res)=>{
+     res.send({ "status": "ok" } )
+})
 app.listen(3001, ()=>{
     console.log("Server running!")
 })
